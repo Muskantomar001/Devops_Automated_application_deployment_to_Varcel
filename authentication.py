0040543eba738120f64abf068a71a21a1c044f62
Muskantomar001 <65162584+Muskantomar001@users.noreply.github.com>
2023-11-03 18:38:58 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -3,7 +3,7 @@ from msrest.authentication import BasicAuthentication
 import pprint
 
 personal_access_token = ''
-url = 'https://dev.azure.com/'
+url = 'https://dev.azure.com/ORG_NAME'
 
 credentials = BasicAuthentication('', personal_access_token)
 connection = Connection(base_url=url, creds=credentials)
