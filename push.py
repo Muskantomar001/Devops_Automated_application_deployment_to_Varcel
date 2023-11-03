e5e82e206ede76bbf904d320cf2365c0e0a559a7
Muskantomar001 <65162584+Muskantomar001@users.noreply.github.com>
2023-11-03 18:50:22 +0530

    Create push.py

---
+++ push.py
@@ -0,0 +1,7 @@
+def push_code_ADO():
+    os.chdir(SERVICE_CODE_PATH)
+    os.system("git init")
+    os.system(f"git remote add origin https://dev.azure.com/{ORG_NAME}/{PROJECT_NAME}/_git/{REPO_NAME}")
+    os.system("git add .")
+    os.system("git commit -m 'Initial commit'")
+    os.system("git push -u origin --all")
